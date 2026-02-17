47702f87cb35a20df039bea7dd54909f9b908ca5
Mayran Adriel <mayranalbuquerque@gmail.com>
2026-02-17 00:06:28 -0300

    Implementando design pattern

---
+++ Challenge/src/Domain/Builders/SalesReportBuilder.cs
@@ -0,0 +1,128 @@
+﻿using Challenge.src.Domain.Entites;
+
+namespace Challenge.src.Domain.Builders
+{
+    public class SalesReportBuilder
+    {
+        private SalesReport _salesReport = new SalesReport();
+
+        public SalesReportBuilder WithTitle(string title)
+        {
+            _salesReport.Title = title;
+            return this;
+        }
+
+        public SalesReportBuilder WithFormat(string format)
+        {
+            _salesReport.Format = format;
+            return this;
+        }
+
+        public SalesReportBuilder WithStartDate(DateTime startDate)
+        {
+            _salesReport.StartDate = startDate;
+            return this;
+        }
+
+        public SalesReportBuilder WithEndDate(DateTime endDate)
+        {
+            _salesReport.EndDate = endDate;
+            return this;
+        }   
+
+        public SalesReportBuilder WithHeaderText(string headerText)
+        {
+            _salesReport.IncludeHeader = true;
+            _salesReport.HeaderText = headerText;
+            return this;
+        }
+
+        public SalesReportBuilder WithFooterText(string footerText)
+        {
+            _salesReport.IncludeFooter = true;
+            _salesReport.FooterText = footerText;
+            return this;
+        }
+
+        public SalesReportBuilder WithChartType(string chartType)
+        {
+            _salesReport.IncludeCharts = true;
+            _salesReport.ChartType = chartType;
+            return this;
+        }
+
+        public SalesReportBuilder WithIncludeSummary(bool includeSummary)
+        {
+            _salesReport.IncludeSummary = includeSummary;
+            return this;
+        }
+
+        public SalesReportBuilder AddColumn(string column)
+        {
+            _salesReport.Columns.Add(column);
+            return this;
+        }
+
+        public SalesReportBuilder AddFilter(string filter)
+        {
+            _salesReport.Filters.Add(filter);
+            return this;
+        }
+        
+        public SalesReportBuilder WithSortBy(string sortBy)
+        {
+            _salesReport.SortBy = sortBy;
+            return this;
+        }
+
+        public SalesReportBuilder WithGroupBy(string groupBy)
+        {
+            _salesReport.GroupBy = groupBy;
+            return this;
+        }
+
+        public SalesReportBuilder WithIncludeTotals(bool includeTotals)
+        {
+            _salesReport.IncludeTotals = includeTotals;
+            return this;
+        }
+
+        public SalesReportBuilder WithOrientation(string orientation)
+        {
+            _salesReport.Orientation = orientation;
+            return this;
+        }
+
+        public SalesReportBuilder WithPageSize(string pageSize)
+        {
+            _salesReport.PageSize = pageSize;
+            return this;
+        }
+
+        public SalesReportBuilder WithIncludePageNumbers(bool includePageNumbers)
+        {
+            _salesReport.IncludePageNumbers = includePageNumbers;
+            return this;
+        }
+
+        public SalesReportBuilder WithCompanyLogo(string companyLogo)
+        {
+            _salesReport.CompanyLogo = companyLogo;
+            return this;
+        }
+
+        public SalesReportBuilder WithWaterMark(string waterMark)
+        {
+            _salesReport.WaterMark = waterMark;
+            return this;
+        }
+
+        public SalesReport Build()
+        {
+            if (string.IsNullOrWhiteSpace(_salesReport.Title)) throw new InvalidOperationException(nameof(_salesReport.Title));
+            if (string.IsNullOrWhiteSpace(_salesReport.Format)) throw new InvalidOperationException(nameof(_salesReport.Format));
+
+            return _salesReport;
+        }
+    }
+}
---
+++ Challenge/src/Program.cs
@@ -0,0 +1,68 @@
+﻿using Challenge.src.Domain.Builders;
+
+namespace Challenge.src
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Sistema de Relatórios ===");
+
+            var report1 = new SalesReportBuilder()
+                .WithTitle("Vendas Mensais")
+                .WithFormat("PDF")
+                .WithStartDate(new DateTime(2024, 1, 1))
+                .WithEndDate(new DateTime(2024, 1, 31))
+                .WithIncludeSummary(true)
+                .AddColumn("Produto")
+                .AddColumn("Quantidade")
+                .AddColumn("Valor")
+                .AddFilter("Status=Ativo")
+                .WithSortBy("Valor")
+                .WithGroupBy("Categoria")
+                .WithIncludeTotals(true)
+                .WithOrientation("Portrait")
+                .WithPageSize("A4")
+                .WithIncludePageNumbers(true)
+                .WithCompanyLogo("logo.png")
+                .WithWaterMark("Confidencial")
+                .Build();
+
+            report1.Generate();
+
+            var report2 = new SalesReportBuilder()
+                .WithTitle("Relatório Trimestral")
+                .WithFormat("Excel")
+                .WithStartDate(new DateTime(2024, 1, 1))
+                .WithEndDate(new DateTime(2024, 3, 31))
+                .AddColumn("Vendedor")
+                .AddColumn("Região")
+                .AddColumn("Total")
+                .WithChartType("Line")
+                .WithHeaderText("Teste")
+                .WithGroupBy("Região")
+                .Build();
+
+
+            report2.Generate();
+
+            var report3 = new SalesReportBuilder()
+                .WithTitle("VendasAnuais")
+                .WithFormat("PDF")
+                .WithStartDate(new DateTime(2024, 1, 1))
+                .WithEndDate(new DateTime(2024, 12, 31))
+                .WithHeaderText("Relatório de Vendas")
+                .WithFooterText("Confidencial")
+                .AddColumn("Produto")
+                .AddColumn("Quantidade")
+                .AddColumn("Valor")
+                .WithChartType("Pie")
+                .WithIncludeTotals(true)
+                .WithOrientation("Landscape")
+                .WithPageSize("A4")
+                .Build();
+
+            report3.Generate();
+        }
+    }
+}
